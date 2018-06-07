3e6e8d0c5b4304a5a36907bc00b7edd14277c439
Anton Chupak <tony.chupak@gmail.com>
2018-06-07 21:53:04 +0300

    change style popup

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -6,26 +6,22 @@ import 'jquery.rateit';
 $( document ).ready(function() {
   const language = document.getElementsByClassName('language');
   const languageEl = document.getElementsByClassName('language__el');
-  const popup = document.getElementById('popup');
   const popupOverlay = document.getElementById('popup-overlay');
+  const popup = document.getElementById('popup');
   const btnPopup = document.getElementById('popup__close');
 
-  $(popupOverlay).fadeIn(400,
-    function(){
-      $(popup)
-        .css('display', 'block')
-        .animate({opacity: 1, top: '50%'}, 200);
-    });
-
-  $(btnPopup, popupOverlay).click( function(){
-    $(popup)
-      .animate({opacity: 0, top: '45%'}, 200,
-        function(){ // пoсле aнимaции
-          $(this).css('display', 'none');
-          $(popupOverlay).fadeOut(400);
-        }
-      );
+  $('body').mouseleave(function(event) {
+    if(event.clientY <= 0 ) {
+      $(popup).fadeIn('slow');
+      $(popupOverlay).fadeIn('slow');
+    }
   });
+
+  $(btnPopup).click(function () {
+    $(popup).toggle();
+    $(popupOverlay).toggle();
+  });
+
 
   $(language).click(function(){
     $(this).toggleClass('is-open');
